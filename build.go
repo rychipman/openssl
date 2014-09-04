3713458969bad9caecff692585643eb1888b6ef2
Your Name <you@example.com>
2014-09-04 22:29:36 +0000

    getting things working on windows with a visual studio built version of openssl

--- build.go
+++ build.go
@@ -16,7 +16,7 @@
 
 package openssl
 
-// #cgo pkg-config: libssl libcrypto
+// #cgo linux pkg-config: openssl
 // #cgo windows CFLAGS: -DWIN32_LEAN_AND_MEAN
 // #cgo darwin CFLAGS: -Wno-deprecated-declarations
 import "C"
